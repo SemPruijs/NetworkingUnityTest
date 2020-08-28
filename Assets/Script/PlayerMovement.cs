14600ced4f9256a661a82e152c822351adc2cd7f
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-08-28 22:45:20 +0200

    Add a player sprite and playermovement script

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+      //Input
+      private float _moveHorizontal;
+      private float _moveVertical;
+
+      private Vector2 _movement;
+      
+      //Speed
+      public float moveSpeed;
+      public float jumpForce;
+      
+      //Data
+      public int amountOfJumps;
+      private int _jumpsLeft;
+      
+      public bool turnAroundAnimation;
+      public bool topDown;
+
+      //Components
+      private Rigidbody2D _rb2d;
+      private AudioSource _audioSource;
+      
+      //Sound
+      [Header("AudioClips")]
+      public AudioClip jumpSound;
+
+      void Start()
+      {
+          _rb2d = GetComponent<Rigidbody2D>();
+          _audioSource = GetComponent<AudioSource>();
+      }
+  
+      [Client]
+      void Update()
+      {
+          _moveHorizontal = Input.GetAxis("Horizontal");
+
+          if (topDown)
+          {
+              _moveVertical = Input.GetAxis("Vertical");   
+              
+              _movement = new Vector2 (_moveHorizontal, _moveVertical);
+          }
+          else
+          {
+              _movement = new Vector2 (_moveHorizontal, 0);
+          }
+          
+
+          if (!topDown)
+          {
+              if (Input.GetKeyDown("space") || Input.GetKeyDown("w") || Input.GetKeyDown("up"))
+              {
+                  if (_jumpsLeft > 0)
+                  {
+                      _jumpsLeft--;
+                  }
+              }
+          }
+          
+          //animation
+          if (turnAroundAnimation)
+          {
+              if (_moveHorizontal < 0)
+              {
+                  transform.rotation = Quaternion.Euler(0, 180, 0);
+              } else if (_moveHorizontal > 0)
+              {
+                  transform.rotation = Quaternion.Euler(0, 0, 0);
+              }
+          }
+      }
+
+      private void OnCollisionEnter2D(Collision2D other)
+      {
+          if (!topDown)
+          {
+              if (other.gameObject.CompareTag("Ground"))
+              {
+                  _jumpsLeft = amountOfJumps;
+              }
+          }
+      }
+
+      [Client]
+      private void FixedUpdate()
+      {
+            _rb2d.AddForce (_movement * moveSpeed);
+      }
+}
